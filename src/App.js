ff8f5e154869b1cb1aca4ef30df364b6cd32b3cc
Shreyas Patankar <ssp1996@gmail.com>
2019-09-18 22:50:54 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import Films from "./films/Films";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Route path="/" exact component={Films} />
+    <Route path="/film-detail/" component={About} />
+    <Route path="/users/" component={Users} />
+</Router>
   );
 }
 
