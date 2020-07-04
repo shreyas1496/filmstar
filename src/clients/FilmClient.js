c3866ae93c3983e9c4e06a1772718b40e846a1d1
Shreyas Patankar <ssp1996@gmail.com>
2020-07-04 12:24:10 +0530

    Fix url

--- src/clients/FilmClient.js
+++ src/clients/FilmClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const FilmClient = {
   getFilms: (search) => axios.get(`films/?search=${search}`),
-  getFilmDetail: (id) => axios.get(`films/${id}`),
+  getFilmDetail: (id) => axios.get(`films/${id}/`),
 };
 
 export default FilmClient;
